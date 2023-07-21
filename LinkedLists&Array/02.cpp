ecf9ece1f22d8abed5c992235a0b57b9694998a8
Shivam Chauhan <63206281+chshivm007@users.noreply.github.com>
2023-07-22 00:02:53 +0530

    Reverting last Commit Done by Accident

--- LinkedLists&Array/02.cpp
+++ LinkedLists&Array/02.cpp
@@ -1,36 +1,98 @@
 /* Problem Link:
-    https://practice.geeksforgeeks.org/problems/minimum-platforms-1587115620/1#
+    https://leetcode.com/problems/copy-list-with-random-pointer/
 */
 
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution{
+struct Node {
+    int val;
+    Node *next, *random;
+
+    Node(int x = 0){
+        val = x;
+        next = NULL;
+        random = NULL;
+    }
+    Node(int x, Node *next) : val(x), next(next) {}
+};
+
+void insert(int ele, Node* &head) {
+    Node* node = new Node(ele);
+
+    if (head == NULL) {
+        head = node;
+        return;
+    }
+
+    Node* iter = head;
+    while (iter -> next != NULL) {
+        iter = iter -> next;
+    }
+    iter -> next = node;
+}
+
+void display(Node* head) {
+    Node* temp = head;
+    int ele = 0;
+    while (temp != NULL) {
+        if (ele > 30){
+            cout << " and so on ...";
+            break;
+        } 
+        cout << temp -> val << " ";
+        temp = temp -> next;
+
+        ++ele;
+    }
+    cout << endl;
+}
+
+class Solution {
 public:
-    int findPlatform(int arr[], int dep[], int n) {
-        sort(arr, arr + n);
-        sort(dep, dep + n);
-        
-        int i = 1, j = 0, platform  = 1, result = 0;
-        while (i < n && j < n) {
-            if (arr[i] <= dep[j]) {
-                ++platform; ++i;
-            }
-            else {
-                --platform; ++j;
-            }
-            result = max(result, platform);
+    Node* copyRandomList(Node* head) {
+        Node* iter = head, *prev = head;
+        while (iter != NULL) {
+            iter = prev -> next;
+            Node* copy = new Node(prev->val);
+            prev -> next = copy;
+            copy -> next = iter;
+            prev = iter;
         }
-        return result;
+
+        iter = head;
+        while (iter != NULL) {
+            if (iter -> random != NULL) {
+                iter -> next -> random = iter -> random -> next;
+            }
+            iter = iter -> next -> next;
+        }
+
+        Node* newHead = new Node(0);
+
+        Node* run = newHead;
+        prev = head, iter = head;
+        while (prev != NULL) {
+            iter = prev -> next;
+            run -> next = iter;
+            prev -> next = iter -> next;
+            run = run -> next;
+            prev = iter -> next;
+        }
+        return newHead -> next; 
     }
 };
 
 int main() {
     int n; cin >> n;
-    int arr[n], dep[n];
+    Node* list = new Node(0);
     for (int i = 0; i < n; ++i) {
-        cin >> arr[i] >> dep[i];
+        int ele; cin >> ele;
+        insert(ele, list);
     }
+
     Solution s;
-    cout << s.findPlatform(arr, dep, n) << endl;
+    auto result = s.copyRandomList(list -> next);
+
+    display(result);
 }

7706f865ba021ce9ad2968e4ac252060ce0247b2
Shivam Chauhan <63206281+chshivm007@users.noreply.github.com>
2023-07-22 14:24:29 +0530

    Update 04.cpp

--- N8-Greedy/04.cpp
+++ N8-Greedy/04.cpp
@@ -1,5 +1,5 @@
 /* Problem Link:
-    https://practice.geeksforgeeks.org/problems/job-sequencing-problem-1587115620/1
+    https://practice.geeksforgeeks.org/problems/fractional-knapsack-1587115620/1
 */
 
 #include <bits/stdc++.h>
